53b53d2b60fff60acedf05e933687cafc1465641
Vedenier <served9@yandex.ru>
2022-05-07 10:54:38 +0300

    attachments upload api works fine

--- api/clickupApiAttachments.service.js
+++ api/clickupApiAttachments.service.js
@@ -1,49 +1,58 @@
 require('dotenv').config();
 const axios = require('axios');
 const token = process.env.CLICKUP_TOKEN;
+const fs = require('fs');
+const FormData = require('form-data');
 
-class postAttachmentsService {
+class PostAttachmentsService {
 
-    static async createTaskAttachment(task_id, form, headers) {
+    static async createTaskAttachment(task_id, ctx) {
+        const form = new FormData();
+        form.append('attachment', fs.createReadStream(`./test/download/${ctx.update.message.message_id}.jpg`))
+        form.append('filename', `${ctx.update.message.message_id}.jpg`)
 
         await axios({
             method: 'post',
             url: `https://api.clickup.com/api/v2/task/${task_id}/attachment`,
             data: form,
-            headers,
-        })
-            .then(() => console.log('Загружено в кликап!'))
-            .catch((e) => console.log(e))
-        // return response
-    }
-
-    static async createCommentAttachment(task_id, urll) {
-
-        await axios({
-            method: 'post',
-            url: `https://api.clickup.com/api/v2/task/${task_id}/comment`,
-            data: {
-                'comment': [
-                    {
-                        "text": urll,
-                        "attributes": {
-                            'attachment': true
-                        }
-                    }
-                ]
-            },
             headers: {
                 'Authorization': token,
-                'Content-Type': 'application/json'
-            }
+                'content-type': `multipart/form-data; boundary=${form.getBoundary()}`
+            },
         })
+
             .then(() => console.log('Загружено в кликап!'))
             .catch((e) => console.log(e))
-        // return response
-    }
+
+    };
+
+    // static async createCommentAttachment(task_id, urll) {
+
+    //     await axios({
+    //         method: 'post',
+    //         url: `https://api.clickup.com/api/v2/task/${task_id}/comment`,
+    //         data: {
+    //             'comment': [
+    //                 {
+    //                     "text": urll,
+    //                     "attributes": {
+    //                         'attachment': true
+    //                     }
+    //                 }
+    //             ]
+    //         },
+    //         headers: {
+    //             'Authorization': token,
+    //             'Content-Type': 'application/json'
+    //         }
+    //     })
+    //         .then(() => console.log('Загружено в кликап!'))
+    //         .catch((e) => console.log(e))
+
+    // }
 
 }
 
 
 
-module.exports = postAttachmentsService
+module.exports = PostAttachmentsService
--- scenes/stepRoute1.scene.js
+++ scenes/stepRoute1.scene.js
@@ -6,52 +6,32 @@ const { Composer } = require('telegraf'),
     axios = require('axios'),
     fs = require('fs'),
     PostAttachmentsService = require('../api/clickupApiAttachments.service'),
-    FormData = require('form-data');
-require('dotenv').config();
+    FormData = require('form-data'),
+    path = require('path');
 
-const token = process.env.CLICKUP_TOKEN;
-const form = new FormData()
-const headers = form.getHeaders();
-headers.authorization = token;
-let urll = undefined
 const stepRoute1 = new Composer()
 
-stepRoute1.on(['document', 'photo'], async (ctx) => {
+stepRoute1.on('photo', async (ctx) => {
     // Берем здесь фотки из сообщения и отправляем в кликап в текущий таск
     const files = ctx.update.message.photo
     fileId = files[3].file_id
-    const url = await ctx.telegram.getFileLink(fileId)
+    let url = await ctx.telegram.getFileLink(fileId)
     url = url.href
+
     const response = await axios({ url, responseType: 'stream' })
     response.data.pipe(fs.createWriteStream(`./test/download/${ctx.update.message.message_id}.jpg`))
 
         .on('finish', async () => {
             console.log(`Файл ${ctx.update.message.message_id}.jpg загружен`)
-            await PostAttachmentsService.createCommentAttachment('2bukvwe', url)
-            // fs.rename(`./test/download/${ctx.update.message.message_id}.jpg`, './test/upload/attachment.jpg', err => {
-            //     if (err) throw err
-            //     console.log('Файл перемещён')
-            //     return url
-            // })
-
+            PostAttachmentsService.createTaskAttachment('2eaj9tf', ctx)
         })
-
         .on('error', e => ctx.reply(`Ошибка, ${e}`))
-
-
-    form.append('attachment', fs.createReadStream(`./test/upload/attachment.jpg`))
-    form.append('filename', `${ctx.update.message.message_id}.jpg`)
-    fs.rmSync(`./test/upload/attachment.jpg`, {
-        force: true,
-        recursive: true,
-        maxRetries: 1
-    })
 
 })
 
 stepRoute1.action('leaveScene', async (ctx) => {
-    // await GetTimeService.stopTimeEntry(24409308)
-    // await GetTasksService.setTaskStatus('2bukvwe', 'to do')
+    await GetTimeService.stopTimeEntry(24409308)
+    await GetTasksService.setTaskStatus('2eaj9tf', 'to do')
     await ctx.deleteMessage()
     await deleteMessagePrev(ctx, 1)
     await sendMessageDriverMenu(ctx)
