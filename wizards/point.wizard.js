2fcf44ad61b96cc2e97140f7edb3260be415b9c9
Vedenier <served9@yandex.ru>
2022-05-16 18:49:44 +0300

    Merge branch 'try' of https://github.com/Ludenser/tg-Bot into try

--- wizards/point.wizard.js
+++ wizards/point.wizard.js
@@ -2,11 +2,16 @@ const
     { Scenes } = require('telegraf'),
     point_scene = require('../scenes/point.scene');
 
+const pointSupplyScene = (role, arr) => {
+    switch (role) {
+        case 'supply':
+            return new Scenes.WizardScene('POINTS_SUPPLY_WIZARD_ID', ...point_scene(arr))
 
-// const arr = ['2bukw4c', '2bukw5j', '2bukw66']
+        case 'clean':
+            return new Scenes.WizardScene('POINTS_CLEAN_WIZARD_ID', ...point_scene(arr))
 
-const pointSupplyScene = (arr) => {
-    return new Scenes.WizardScene('POINTS_SUPPLY_WIZARD_ID', ...point_scene(arr))
+    }
+
 }
 
 module.exports = pointSupplyScene
