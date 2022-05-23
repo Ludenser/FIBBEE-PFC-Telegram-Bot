4af5b9bc77a06aaf124a907172a23714a104fb07
Vedenier <served9@yandex.ru>
2022-05-23 16:34:27 +0300

    cosmetics in point.scenes

--- scenes/point.scene.js
+++ scenes/point.scene.js
@@ -17,14 +17,30 @@ module.exports = (arr) => {
             const point_scene = new Composer()
 
             point_scene.action('enter', async (ctx) => {
+                try {
+                    await GetTasksService.setTaskStatus(task.id, 'in progress')
+                    await GetTimeService.startTimeEntry(ctx.team_id, task.id)
+                } catch (e) {
+                    await sendMessageError(ctx, e)
+                }
                 await ctx.deleteMessage()
-                // try {
-                //     await GetTasksService.setTaskStatus(task.id, 'in progress')
-                //     await GetTimeService.startTimeEntry(ctx.team_id, task.id)
-                // } catch (e) {
-                //     await sendMessageError(ctx, e)
-                // }
+                await ctx.reply(task.name,
+                    Markup.inlineKeyboard(
+                        [
+                            Markup.button.callback('Загрузить фото', 'upl_photo'),
+                            Markup.button.callback('Оставить комментарий', 'upl_comment'),
+                            Markup.button.callback('Закончить обслуживание', 'next_step'),
+                            Markup.button.callback('Выйти', 'leaveScene')
+                        ], {
+                        wrap: (btn, index, currentRow) => currentRow.length >= (index + 1) / 2
+                    })
+                )
+                console.log(task.name, task.id)
+            })
 
+            point_scene.action('enter_more', async (ctx) => {
+                await ctx.deleteMessage()
+                await deleteMessagePrev(ctx, 1)
                 await ctx.reply(task.name,
                     Markup.inlineKeyboard(
                         [
@@ -51,10 +67,10 @@ module.exports = (arr) => {
                         Markup.button.callback('Подтвердить загрузку фото✅')
                     ]).resize(true).oneTime(true)
                 )
-                await ctx.reply(task.name,
+                await ctx.reply(`Прикрепляем фото к таску ${task.name}`,
                     Markup
                         .inlineKeyboard([
-                            Markup.button.callback('Вернуться в меню осблуживания комплекса', 'enter'),
+                            Markup.button.callback('Вернуться в меню осблуживания комплекса', 'enter_more'),
                         ]))
                 point_scene.on('photo', async (ctx) => {
                     await postAttachmentsFeature(ctx, task.id)
@@ -62,7 +78,11 @@ module.exports = (arr) => {
             })
 
             point_scene.action('upl_comment', async (ctx) => {
-                await ctx.reply('Напиши комментарий к таску, если нужно кого-то тегнуть, добавь @nickname')
+                await ctx.reply('Напиши комментарий к таску, если нужно кого-то тегнуть, добавь @nickname',
+                    Markup
+                        .inlineKeyboard([
+                            Markup.button.callback('Вернуться в меню осблуживания комплекса', 'enter_more'),
+                        ]))
                 point_scene.on('message', async (ctx) => {
                     await postCommentFeature(ctx, task.id)
                     await ctx.deleteMessage()
