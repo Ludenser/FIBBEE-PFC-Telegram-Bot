6ffc8a340dfbd4d32e9a15ccfcc896dec43e770f
Sergei Vedeneev <served9@yandex.ru>
2022-08-03 16:32:15 +0300

    gitignore

--- composers/start.composer.js
+++ composers/start.composer.js
@@ -11,7 +11,7 @@ const chalk = require('chalk')
 const composer = new Composer();
 
 composer.start(async (ctx) => {
-  ctx.session = null
+
   composer.use(async (ctx, next) => {
     if (!ctx.session.isAlreadyFilled) {
       console.log(chalk.whiteBright.bgRed('ctx.session is empty'))
@@ -29,6 +29,7 @@ composer.start(async (ctx) => {
 
 composer.command('/start', async (ctx) => {
   try {
+    ctx.session = null
     await sendMessageStart(ctx)
   } catch (e) {
     await sendMessageError(ctx, e)
