34f318c919cba07a594592775463b3e4853c5d50
Ludenser <served9@yandex.ru>
2022-08-13 18:44:22 +0300

    file upload stages, heroku test 13.08

--- composers/start.composer.js
+++ composers/start.composer.js
@@ -1,11 +1,14 @@
 const { Composer } = require('telegraf');
 const sendMessageStart = require('../keyboards/mainMenu/sendMessageStart');
-const { sendError } = require('../utils/sendLoadings');
+const { sendError, sendProses } = require('../utils/sendLoadings');
 const addTasksToCtx = require('../features/addTasksToCtx.feature');
 const convertTranslit = require('cyrillic-to-translit-js')
 const chalk = require('chalk');
 const supplyTeam_ids = require('../lib/supplyTeam_ids');
 const _ = require('lodash');
+const authUserFeature = require('../features/authUser.feature');
+const totalSceneInitComposer = require('./totalSceneInit.composer');
+const selectRouteComposer = require('./selectRoute.composer');
 
 /**
   * Обработчик стартовых команд.
@@ -17,16 +20,14 @@ const composer = new Composer();
 const cyrillicToTranslit = new convertTranslit();
 
 composer.start(async (ctx) => {
+
   ctx.session = null
+  await ctx.deleteMessage()
   const userName = `${ctx.update.message.from.first_name} ${ctx.update.message.from.last_name}`
   ctx.session.userName = cyrillicToTranslit.transform(userName)
+
   ctx.session.isAuthUser = false
-  const userIdMatch = _(supplyTeam_ids)
-    .find(['username', ctx.session.userName])
-  ctx.session.user = userIdMatch
-  if (ctx.session.user) {
-    ctx.session.isAuthUser = true
-  }
+  await authUserFeature(ctx.session)
 
   try {
     await sendMessageStart(ctx)
@@ -41,28 +42,47 @@ composer.start(async (ctx) => {
       console.log(chalk.whiteBright.bgRed('ctx.session is empty'))
       await addTasksToCtx(ctx)
       console.log(chalk.blackBright.bgGreen('ctx.session was filled'))
+      composer.use(totalSceneInitComposer(ctx))
+      composer.use(... (ctx))
     }
     await next()
   })
 
 })
 
-composer.command('/start', async (ctx) => {
-  try {
-    ctx.session = null
-    await sendMessageStart(ctx)
-  } catch (e) {
-    await sendError(ctx, e)
-  }
-})
-
 composer.action('start', async (ctx) => {
   ctx.session = null
+  await ctx.deleteMessage()
+  const userName = `${ctx.update.callback_query.from.first_name} ${ctx.update.callback_query.from.last_name}`
+  ctx.session.userName = cyrillicToTranslit.transform(userName)
+
+  ctx.session.isAuthUser = false
+  await authUserFeature(ctx.session)
+
   try {
     await sendMessageStart(ctx)
   } catch (e) {
     await sendError(ctx, e)
   }
+
+  composer.use(async (ctx, next) => {
+
+    if (!ctx.session.isAlreadyFilled && ctx.session.isAuthUser) {
+
+      console.log(chalk.whiteBright.bgRed('ctx.session is empty'))
+      await addTasksToCtx(ctx)
+      console.log(chalk.blackBright.bgGreen('ctx.session was filled'))
+      composer.use(totalSceneInitComposer(ctx))
+      composer.use(...selectRouteComposer(ctx))
+    }
+    await next()
+  })
+
+})
+
+composer.on('text', async (ctx) => {
+  await ctx.deleteMessage()
+  await sendProses(ctx, 'Тут такое не приветствуется.')
 })
 
 module.exports = composer
