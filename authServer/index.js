f437a1e71d1cd84cc5f5fa8b43a13a270c83a9bc
Sergei Vedeneev <served9@yandex.ru>
2022-09-12 18:46:23 +0300

    heroku app name in server

--- authServer/index.js
+++ authServer/index.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const ClientOAuth2 = require('client-oauth2')
-
-
 require('dotenv').config();
+
+const PORT = process.env.PORT || 3000
+const APP_NAME = process.env.HEROKU_APP_NAME || 'telegrambottest'
 
 let clickupAuth = new ClientOAuth2({
   clientId: 'PJERCGBWE7XIVE0XS8N1R6R0MQCV6TK2',
   clientSecret: 'H55Q9B18O1S97NKUKL65MO0B77ULPBOZP18KYFXVXR04596XS5X5YJJSO7B33VO7',
   accessTokenUri: 'https://app.clickup.com/api/v2/oauth/token',
   authorizationUri: 'https://app.clickup.com/api',
-  redirectUri: 'https://telegrambottest.herokuapp.com/auth/callback'
+  redirectUri: `https://${APP_NAME}.herokuapp.com/auth/callback`
 })
 
+const app = express()
 
-const PORT = process.env.PORT || 3000
-let app = express()
 app.listen(PORT, () => {
   console.log(`Server is starting by ${PORT} port `);
 })
@@ -30,5 +30,4 @@ app.get('/auth/callback', function (req, res) {
   const tokenString = req.originalUrl.match(reg)
   console.log(tokenString[0]);
   res.redirect(`https://t.me/pfctest_bot?start=${tokenString[0]}`)
-
 })
