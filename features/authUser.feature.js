95d403901709aced25dc38d8813b165562fef626
Sergei Vedeneev <served9@yandex.ru>
2022-09-12 18:05:03 +0300

    add ctx to auth user

--- features/authUser.feature.js
+++ features/authUser.feature.js
@@ -2,17 +2,17 @@ const _ = require('lodash');
 const supplyTeam_ids = require('../lib/supplyTeam_ids');
 /**
   * Функция для поиска telegram username текущего пользователя в списке зарегистрированных пользователей из файла lib/supplyTeam_ids
-  * @param {Array} session - массив с текущей сессией
+  * @param {Ctx} ctx - объект контекста telegraf
   */
-module.exports = async (session) => {
+module.exports = async (ctx) => {
     const userIdMatch = _(supplyTeam_ids)
-        .find(['username', session.userName])
-    session.user = userIdMatch
+        .find(['username', ctx.session.userName])
+    ctx.session.user = userIdMatch
 
-    if (session.user) {
+    if (ctx.session.user) {
         if (ctx.hasOwnProperty('startPayload')) {
-            session.user.CU_Token = ctx.startPayload
-            session.isAuthUser = true
+            ctx.session.user.CU_Token = ctx.startPayload
+            ctx.session.isAuthUser = true
         }
     }
 }
