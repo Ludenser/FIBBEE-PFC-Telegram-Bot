98ba795ad21ea68593ef072fcbb500269af73c1c
Sergei Vedeneev <86262038+Ludenser@users.noreply.github.com>
2022-09-23 11:08:16 +0300

    update db url

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize')
 require('dotenv').config();
-const DB_URL = process.env.DATABASE_URL || 'postgres://vqktkafkooesla:ebba53cb1550efbef5e2d5ceed9fc78421d88db7da4864b2789b7539ecb5c8c0@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/daopoms87fdmb5'
+const DB_URL = process.env.DATABASE_URL || 'postgres://zaxtucpcfpjjhq:779d60497c21bc93af1e9a1240d02ac2b0db9a473928289c5ed1465e1bf453f1@ec2-54-220-86-118.eu-west-1.compute.amazonaws.com:5432/ddm2155oflmhtp'
 
 module.exports = new Sequelize(
   DB_URL,
